c7f8b6ec4be66808ac2b3118e23e8faee2d1d0bd
digital-nishtyak <lapaev.yarik@mail.ru>
2023-11-13 18:10:17 +0300

    laba 16 done

---
+++ laba_16/src/main/java/AnimalListServlet.java
@@ -0,0 +1,30 @@
+import org.example.AnimalList;
+
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class AnimalListServlet extends HttpServlet {
+
+    private AnimalList animalList;
+
+    public void init() {
+        animalList = new AnimalList();
+        animalList.readFromFile();
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/plain;charset=UTF-8");
+
+        request.setAttribute("animals", animalList.getAnimals());
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/main.jsp");
+        dispatcher.forward(request, response);
+    }
+}
---
+++ laba_16/src/main/java/org/example/AnimalList.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AnimalList {
+
+    private final Map<String, List<String>> animals;
+
+    public AnimalList() {
+        animals = new HashMap<>();
+    }
+
+    public void readFromFile() {
+        String userDir = FileSystems.getDefault()
+                .getPath("")
+                .toAbsolutePath()
+                .getParent()
+                .toString();
+        File file = new File(userDir + "/webapps/laba_16/src/main/resources/animals.txt");
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parsedLine = line.split(":");
+                List<String> parsedAnimals = List.of(parsedLine[1].split(","));
+                animals.put(parsedLine[0], parsedAnimals);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Map<String, List<String>> getAnimals() {
+        return animals;
+    }
+}
